afa2d00cca76323ce38f69ae69c4443f1c16772b
Surya Dev Rajesh <srsuryadev@gmail.com>
2016-08-10 19:00:46 +0530

    Update Rummy.java
    added check group

--- src/Rummy.java
+++ src/Rummy.java
@@ -23,6 +23,20 @@ public class Rummy {
 		return result;
 	}
 	
+	public static boolean checkGroup(Hand hand, int n){
+		hand.sortRank();
+		int count = 0;
+		for(int j = 0; j < hand.cards.size()-1 ; j++){
+			int r = hand.cards.get(j).getrank();
+			int nextr = hand.cards.get(j+1).getrank();
+			if(r==-1||r == nextr){
+				count++;
+			}
+		}
+		if(count == n)
+			return true;
+		return false;			
+	}
 	
 	public int checkSeq(Hands hand){
 		Hands h = new Hands();
